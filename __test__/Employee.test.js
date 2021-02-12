1ea3bf20d794af133422554d66c9296fc3ee9725
Teangelo Burks <burks.teangelo@gmail.com>
2021-02-11 22:32:40 -0500

    First test case is complete

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,1 +1,10 @@
-const employee = require('../')
+const Employee = require('../lib/employee');
+
+describe("employee", () => {
+    describe("Initialize new instance of employee class", () => {
+        it("Should make a new employee", () => {
+            const mainEmp = new Employee;
+            expect(mainEmp).toEqual(new Employee)
+        })
+    })
+})
